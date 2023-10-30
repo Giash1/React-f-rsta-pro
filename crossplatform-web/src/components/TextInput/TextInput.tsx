f100966790fe8675a4b7430558eb438f476a91d1
Giash ITH <89529541+Giash1@users.noreply.github.com>
2023-10-30 20:34:12 +0100

    commend out two buttons

--- crossplatform-web/src/components/TextInput/TextInput.tsx
+++ crossplatform-web/src/components/TextInput/TextInput.tsx
@@ -9,9 +9,9 @@ export function TextInput(props) {
         onInput,
         value,
         styling,
-        onButtonClick,
+        // onButtonClick,
         onButtonClickClear,
-        setLastName
+        // setLastName
     } = props
     //  example of destructuring/adding
     // const arr1 =['orange', 'banana']
@@ -26,7 +26,7 @@ export function TextInput(props) {
                 // adding darkmode variable from the setdarkmode props to the styling using distructuring ...
                 style={{
                     ...styling,
-                    // if it darkmode color will be white otherwise white
+                    // if it darkmode color will be black otherwise white
                     backgroundColor: darkMode ? 'black' : 'white'
                     // go to the parentComponent and remove back
                 }}
@@ -37,20 +37,20 @@ export function TextInput(props) {
             />
             {/* {childComponent} */}
             {/* using onButtonClick props as a function */}
-            {onButtonClick && (
-                // associate the onButtonClick function with the button's onClick event
+            {/* {onButtonClick && ( */}
+                {/* // associate the onButtonClick function with the button's onClick event
                 // if you click the button it will show in the console button is clicked
-                <button onClick={onButtonClick}>Hi</button>
-            )}
+            //     <button onClick={onButtonClick}>Hi</button> */}
+            {/* // )} */}
             {/* onButtonClickClear props use as a function and it will clear the firstname  */}
             {onButtonClickClear && (
                 <button onClick={onButtonClickClear}>Clear</button>
             )}
 
             {/* to change the lastname using setLasName function */}
-            {setLastName && (
+            {/* {setLastName && (
                 <button onClick={()=>setLastName('Alim')}>Change lastname</button>
-            )}
+            )} */}
             {/* creating another button for setDarMode useState */}
             <button onClick={() => setDarkMode(!darkMode)}>Change theme</button>
         </div>
